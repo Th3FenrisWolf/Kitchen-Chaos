fe9cf5efbb03afedb82fbe29336d47212141c89d
Ryan Albrecht <ralbrecht@cedarville.edu>
2021-10-28 21:57:06 -0400

    fix animation and add basic respawn mechanic

--- Assets/Scripts/navigation.cs
+++ Assets/Scripts/navigation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class navigation : MonoBehaviour
 {
@@ -10,16 +11,39 @@ public class navigation : MonoBehaviour
     public Transform[] patrolAnchors;
     NavMeshAgent agent;
     private int index = 0;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    IEnumerator ReloadScene() {
+        yield return new WaitForSeconds(2);
+
+        // reload scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // player dead
+        if (Vector3.Distance(agentTrans.position, target.position) < 2) {
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
+            agent.ResetPath();
+
+            // TODO play eat animation and show death/respawn option on screen
+            anim.SetTrigger("eat");
+
+            // lock the player movement
+            StartCoroutine(ReloadScene());
+        }
+
+
         if (Vector3.Distance(agentTrans.position, target.position) >= 2 && Vector3.Distance(agentTrans.position, target.position) <= 15)
         {
             agent.SetDestination(target.position);
