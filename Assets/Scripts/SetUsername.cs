54de8961a60adbaefd7174a786d961b812a38277
Ryan Albrecht <ralbrecht@cedarville.edu>
2021-12-03 01:26:21 -0500

    username is now based on time

---
+++ Assets/Scripts/SetUsername.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetUsername : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Text>().text = System.DateTime.Now.ToString("hhmmss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
